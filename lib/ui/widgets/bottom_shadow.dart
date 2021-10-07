e8219be5927c32e1ff64888742b368ed75ac6d3f
Elias Reis <eliasreis54@gmail.com>
2021-10-06 21:43:25 -0300

    initial ui widgets

---
+++ lib/ui/widgets/bottom_shadow.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BottomShadow extends StatelessWidget {
+  const BottomShadow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 210,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        gradient: LinearGradient(
+          colors: [
+            Colors.black,
+            Colors.transparent,
+            Colors.transparent,
+            Colors.black87,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0, 0, 0.5, 1],
+        ),
+      ),
+    );
+  }
+}
